b0c137d1fa28877cc3cca029904e276caab73348
VeraMikhailova <150927067+VeraMikhailova@users.noreply.github.com>
2024-05-06 07:39:36 +0300

    Update output_direction.cpp

--- analytical/output_direction.cpp
+++ analytical/output_direction.cpp
@@ -6,103 +6,114 @@ using namespace filesystem;
 constexpr double PI = 3.14159265358979323846;
 
 int main() {
-  cout << "Input a name of initial image (.jpg) and label (.txt) (after using "
-          "the model or after using analytical script). Results will be in "
-          "file direction.txt.\n"
-       << flush;
-  string name_jpg, name_txt;
-  cin >> name_jpg >> name_txt;
-  if (!exists(name_jpg) || !exists(name_txt)) {
-    cout << "These files do not exist!\n" << flush;
-    return 0;
-  }
-  copy_file(name_jpg, "tmp.jpg");
+	cout << "Input a name of initial image (.jpg) and label (.txt) (after using "
+		"the model or after using analytical script). Results will be in "
+		"file direction.txt.\nJPG: "
+		<< flush;
+	string name_jpg, name_txt;
+	cin >> name_jpg;
+	cout << "TXT: " << flush;
+	cin >> name_txt;
+	if (!exists(name_jpg) || !exists(name_txt)) {
+		cout << "These files do not exist!\n" << flush;
+		return 0;
+	}
+	if(exists("tmp.jpg")){
+		remove("tmp.jpg");
+	}
+	copy(name_jpg, "tmp.jpg");
 
-  //яркость пикселя
-  double c;
-  system("python3 convert_cv.py");
-  ifstream ifsimg("tmp1.txt");
-  int IMG_H;
-  ifsimg >> IMG_H;
-  vector<vector<double>> img(IMG_H);
-  for (int i = 0; i < IMG_H; ++i) {
-    for (int j = 0; j < IMG_H; ++j) {
-      ifsimg >> c;
-      img[i].push_back(c);
-    }
-  }
-  ifstream ifstxt(name_txt);
-  string text;
-  while (getline(ifstxt, text, ',')) {
+	//яркость пикселя
+	double c;
 
-    getline(ifstxt, text, ',');
-    string tmp;
-    for (int j = 0; j < text.size(); ++j) {
-      if (text[j] != ' ' && text[j] != ',') {
-        tmp.push_back(text[j]);
-      }
-    }
-    double X = stoi(tmp);
-    getline(ifstxt, text, ',');
-    string tmp;
-    for (int j = 0; j < text.size(); ++j) {
-      if (text[j] != ' ' && text[j] != ',') {
-        tmp.push_back(text[j]);
-      }
-    }
-    double Y = stoi(tmp);
-    getline(ifstxt, text, ',');
-    string tmp;
-    for (int j = 0; j < text.size(); ++j) {
-      if (text[j] != ' ' && text[j] != ',') {
-        tmp.push_back(text[j]);
-      }
-    }
-    double W = stoi(tmp);
-    getline(ifstxt, text, ',');
-    string tmp;
-    for (int j = 0; j < text.size(); ++j) {
-      if (text[j] != ' ' && text[j] != ',') {
-        tmp.push_back(text[j]);
-      }
-    }
-    double H = stoi(tmp);
+	system("python3 convert_cv.py");
+	ifstream ifsimg("tmp1.txt");
+	ofstream ofs("direction.txt");
+	int IMG_H;
+	ifsimg >> IMG_H;
+	vector<vector<double>> img(IMG_H);
+	for (int i = 0; i < IMG_H; ++i) {
+		for (int j = 0; j < IMG_H; ++j) {
+			ifsimg >> c;
+			img[i].push_back(c);
+		}
+	}
+	ifstream ifstxt(name_txt);
+	string text;
+	while (getline(ifstxt, text, ',')) {
 
-    //шаг сектора
-    double b = 2 * PI / 100;
-    double x, y;
-    vector<double> sum(100);
-    for (int i = 0; i < IMG_H; ++i) {
-      for (int j = 0; j < IMG_H; ++j) {
-        int c = img[i][j];
-        x = j - X;
-        if (i <= Y) {
-          y = Y - i;
-        } else {
-          y = (-1) * (i - Y);
-        }
+		getline(ifstxt, text, ',');
+		string tmp;
+		for (int j = 0; j < text.size(); ++j) {
+			if (text[j] != ' ') {
+				tmp.push_back(text[j]);
+			}
+		}
+		int X = stoi(tmp);
+		getline(ifstxt, text, ',');
+		tmp="";
+		for (int j = 0; j < text.size(); ++j) {
+			if (text[j] != ' ') {
+				tmp.push_back(text[j]);
+			}
+		}
+		int Y = stoi(tmp);
+		getline(ifstxt, text, ',');
+		tmp="";
+		for (int j = 0; j < text.size(); ++j) {
+			if (text[j] != ' ') {
+				tmp.push_back(text[j]);
+			}
+		}
+		double W = stoi(tmp);
+		getline(ifstxt, text);
+		tmp="";
+		for (int j = 0; j < text.size(); ++j) {
+			if (text[j] != ' ') {
+				tmp.push_back(text[j]);
+			}
+		}
+		double H = stoi(tmp);
+		//cout<<X<<","<<Y<<endl;
+		//cout<<W<<"x"<<H<<endl;
 
-        double a = atan2(y, x);
-        if (a < 0) {
-          a += 2 * PI;
-        }
-        sum[(int)(a / b)] += c;
-      }
-    }
-    double max_sector = 0;
-    int imax = 0;
-    for (int i = 0; i < 100; ++i) {
-      if (sum[i] > max_sector) {
-        max_sector = sum[i];
-        imax = i;
-      }
-    }
+		double r = W * W + H * H;
+		r/=4;
 
-    double angle = imax * (b - 1) + b / 2;
+		//шаг сектора
+		double b = 2 * PI / 100;
+		double x, y;
+		vector<double> sum(100);
+		for (int i = 0; i < IMG_H; ++i) {
+			for (int j = 0; j < IMG_H; ++j) {
+				if(j == X && i == Y){
+					continue;
+				}
+				x = j - X;
+				y = i - Y;
+				if(x * x + y * y > r){
+					continue;
+				}
+				double a = atan2(y, x);
+				if (a < 0) {
+					a += 2 * PI;
+				}
+				sum[(int)(a / b)] += img[i][j];
+			}
+		}
+		double max_sector = 0;
+		int imax = 0;
+		for (int i = 0; i < 100; ++i) {
+			if (sum[i] > max_sector) {
+				max_sector = sum[i];
+				imax = i;
+			}
+		}
 
-    ofstream ofs("direction.txt");
-    ofs << to_string(angle);
-  }
+		double angle = (imax - 1) * b + b / 2;
 
-  return 0;
+		ofs << to_string(angle) << '\n';
+	}
+
+	return 0;
 }
